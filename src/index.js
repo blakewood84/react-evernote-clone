99914f7345e67ff02f874dc1ab25f9a857d94cd5
blakewood84 <60270366+blakewood84@users.noreply.github.com>
2020-07-22 21:14:11 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyBh5ql_hON-nOkiDx8hkDL4BBt16rxQFK4",
+  authDomain: "evernote-clone-c40b0.firebaseapp.com",
+  databaseURL: "https://evernote-clone-c40b0.firebaseio.com",
+  projectId: "evernote-clone-c40b0",
+  storageBucket: "evernote-clone-c40b0.appspot.com",
+  messagingSenderId: "117446773669",
+  appId: "1:117446773669:web:5e92c7bb61f2357a1bb64c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
